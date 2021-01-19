cde2f4827cd9f9dc1590d037384867134e3e897e
botzzz3 <77111954+botzzz3@users.noreply.github.com>
2021-01-19 15:01:07 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,61 @@
+const help = (prefix) => {
+	return `🤖⚠️🤖⚠️⚠️⚠️🤖⚠️🤖
+	
+	                
+
+┃
+┣━━━°❀ ❬ 𝗠𝗔𝗞𝗘𝗥 ❭ ❀°━━━⊱
+┃
+┣➥ *Adesivo $ {prefix} * [foto]
+┣➥ *${prefix} stickergif * [foto]
+┣➥ *${prefix} adesivo nobg
+┣➥ *${prefix} trovão * [texto]
+┣➥ *${prefix} tsticker * [text / url]
+┃
+┣━━━━°❀ ❬ 𝙈𝙀𝘿𝙄𝘼 ❭ ❀°━━━⊱
+┃
+┣➥ *${prefix} tts * [texto]
+┣➥ *${prefix} ocr *
+┣➥ *${prefix} loli *
+┣➥ *${prefix} toimg *
+┣➥ *${prefix} meme *
+┣➥ *${prefix} memeindo *
+┣➥ *${prefix} nsfwloli *
+┣➥ *${prefix} espere * [foto]
+┣➥ *${prefix} simi
+┣➥ *${prefix} simih *
+┣➥ *${prefix} espere *
+
+┣━━━━°❀ ❬ 𝙂𝙍𝙊𝙐𝙋 ❭ ❀°━━━━⊱
+┃
+┣{prefix} setname *
+┣➥ * $ {prefix}setdesc *
+┣➥ * $ {prefix}getpp *
+┣➥ * $ {prefix}tagall *
+┣➥ * $ {prefix}grupo de links
+┣➥ * $ {prefix}gprofile *
+┣➥ * $ {prefix}setprefix
+┣➥ * $ {prefix}bem-vindo *
+┣➥ * $ {prefix}restante *
+┃
+┣━━━━°❀ ❬ 𝙎𝙊𝙐𝙉𝘿 ❭ ❀°━━━━━⊱
+┃
+┣➥ *saudações*
+┣➥ *puxar*
+┣➥ *baka*
+┣➥ *suspiro*
+┣➥ *estupido*
+┣➥ *pao*
+┣➥ *welot*
+┣➥ *irmao *
+┃
+┣━━━━━━━━━━━━━━━━━━━━
+┃criado por: charope,malokeiro 
+┣━━━━━━━━━━━━━━━━━━━━
+┃ 🤖⚠️🤖⚠️🤖⚠️🤖⚠️🤖⚠️🤖⚠️🤖⚠️
+┗━━━━━━━━━━━━━━━━━━━━`
+}
+
+exports.help = help
+
+
